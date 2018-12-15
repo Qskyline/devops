dbe56b35860c0c46480f79faabf87a24678b4291
天际 <qrjskyline@gmail.com>
2018-12-15 17:13:21 +0800

    bug fix

--- src/main/java/com/skyline/service/devops/Devops.java
+++ src/main/java/com/skyline/service/devops/Devops.java
@@ -4,9 +4,11 @@ import com.skyline.platform.core.Run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @ServletComponentScan
 @SpringBootApplication
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class Devops {
     public static void main(String[] args) {
         Class[] classes = new Class[2];
