59e2500eba73a11558c83623adf0a3c3463999d3
天际 <qrjskyline@gmail.com>
2018-12-20 18:45:41 +0800

    bug fix

--- src/main/java/com/skyline/service/devops/Devops.java
+++ src/main/java/com/skyline/service/devops/Devops.java
@@ -4,11 +4,13 @@ import com.skyline.platform.core.Run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @ServletComponentScan
 @SpringBootApplication
 @EnableGlobalMethodSecurity(jsr250Enabled = true)
+@ComponentScan("com.skyline")
 public class Devops {
     public static void main(String[] args) {
         Class[] classes = new Class[2];
--- src/test/java/com/skyline/service/devops/controller/MachineControllerTest.java
+++ src/test/java/com/skyline/service/devops/controller/MachineControllerTest.java
@@ -1,41 +1,69 @@
 package com.skyline.service.devops.controller;
 
 import com.skyline.platform.core.Run;
-import com.skyline.platform.core.model.ResponseModel;
 import com.skyline.service.devops.Devops;
 import com.skyline.service.devops.MySpringJUnit4ClassRunner;
 import net.sf.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.HandlerInterceptor;
+import javax.servlet.http.Cookie;
 
 @RunWith(MySpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {Devops.class, Run.class})
 @WebAppConfiguration
 @Transactional
 public class MachineControllerTest {
+    private MockMvc mockMvc;
+
     @Autowired
     MachineController machineController;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void before() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
-    public void addMachine() {
+    @WithMockUser(authorities = {"user"})
+    public void addMachine() throws Exception {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("loginType", "password");
-        jsonObject.put("ip", "172.18.5.42");
-        jsonObject.put("port", "22");
+        jsonObject.put("ip", "172.18.5.109");
+        jsonObject.put("loginPort", "22");
         jsonObject.put("loginUser", "kduser");
         jsonObject.put("loginPassword", "Kingdee@2018");
         jsonObject.put("tags","redis");
 
-        System.out.println(machineController.addMachine(jsonObject).getErrMsg());
-    }
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .post("/security/addMachine.do")
+                .content(jsonObject.toString())
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .cookie(new Cookie("SESSION", "MmU4MGJhMDgtYTQ2ZC00Y2QzLWE0MzItMzJkZTVjODEzYzlm"))
+        ).andReturn();
 
-    @Test
-    public void getAllMachine() {
-        ResponseModel responseModel = machineController.getAllMachine();
-        System.out.println(responseModel.getErrMsg());
+        HandlerInterceptor[] interceptors = mvcResult.getInterceptors();
+        for (HandlerInterceptor h : interceptors) {
+            System.out.println(h.getClass().getName());
+        }
+        String responseString = mvcResult.getResponse().getContentAsString();
+        System.out.println("返回内容："+responseString);
+        int status = mvcResult.getResponse().getStatus();
+        Assert.assertEquals("return status not equals 200", 200, status);
     }
 }
